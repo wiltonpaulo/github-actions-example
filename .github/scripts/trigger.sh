ebf087a6ed9c44be687e147befe72c5d79d4e8a6
Wilton Paulo <wiltonpaulo@gmail.com>
2022-05-04 13:49:30 -0300

    fix trigger script

--- .github/scripts/trigger.sh
+++ .github/scripts/trigger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WK_LIST = `gh workflow list | grep example | awk {'print $1'}`
-for workflow in WK_LIST; do
-    gh workflow run workflow
+WK_LIST=`gh workflow list | grep example | awk {'print $1'}`
+for workflow in $WK_LIST; do
+    gh workflow run $workflow
 done
